e331c69e7100e76ba0a4bb5615391cf0bccda728
lizgrant <lizgrant@bookmark.home>
2016-08-13 19:32:13 -0500

    Initial Commit

---
+++ BaseStatArrayStructureTests/BaseStatArrayStructureTests.swift
@@ -0,0 +1,36 @@
+//
+//  BaseStatArrayStructureTests.swift
+//  BaseStatArrayStructureTests
+//
+//  Created by lizgrant on 8/13/16.
+//  Copyright © 2016 lizgrant. All rights reserved.
+//
+
+import XCTest
+@testable import BaseStatArrayStructure
+
+class BaseStatArrayStructureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
